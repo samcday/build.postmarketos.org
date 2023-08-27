samcday/build.postmarketos.org

commit d88ecb5e3674b90200585c5e60cfce48c35127d7

Fix WIP repo path for staging
Change the path for WIP repositories of staging branches. Instead of,
for example for a branch called master_staging_test:
    /wip/master_staging_test/x86_64/…
Put it here:
    /staging/wip/test/master/x86_64/…

This fixes that pmbootstrap can use the proper WIP repository during
build, before this patch it used the orignal repositories WIP repo.

bpo/job_services/sourcehut.py
@@ -53,6 +53,12 @@ def get_manifest(name, tasks, branch):
     url_api = bpo.config.args.url_api
     url_repo_wip_http = bpo.config.args.url_repo_wip_http + "/"
     url_repo_wip_https = bpo.config.args.url_repo_wip_https + "/"
+
+    if "_staging_" in branch:
+        branch_orig, name = bpo.repo.staging.branch_split(branch)
+        url_repo_wip_http += f"staging/{name}/"
+        url_repo_wip_https += f"staging/{name}/"
+
     branches = bpo.repo.staging.get_branches_with_staging()
     arches = " ".join(branches[branch]["arches"])
     ret = """

bpo/job_services/local.py
@@ -78,9 +78,14 @@ class LocalJobServiceThread(threading.Thread):
         pmaports = bpo.config.args.local_pmaports
         pmbootstrap = bpo.config.args.local_pmbootstrap
         token = bpo.config.const.test_tokens["job_callback"]
-        repo_wip_path = bpo.config.args.repo_wip_path
         repo_wip_key = bpo.config.const.repo_wip_keys + "/wip.rsa"
         uid = bpo.config.const.pmbootstrap_chroot_uid_user
+
+        repo_wip_path = f"{bpo.config.args.repo_wip_path}/{branch}"
+        if "_staging_" in branch:
+            branch_orig, name = bpo.repo.staging.branch_split(branch)
+            repo_wip_path = f"{bpo.config.args.repo_wip_path}/staging/{name}/{branch_orig}"
+
         return """
             # Remove old temp dir
             temp_dir=""" + shlex.quote(temp_path) + """
@@ -111,9 +116,9 @@ class LocalJobServiceThread(threading.Thread):
             work_path="$(pmbootstrap -q config work)"
             packages_path="$work_path/packages"
             repo_wip_path=""" + shlex.quote(repo_wip_path) + """
-            if [ -n "$branch" ] && [ -d "$repo_wip_path/$branch" ]; then
+            if [ -n "$branch" ] && [ -d "$repo_wip_path" ]; then
                 sudo mkdir -p "$packages_path/$channel"
-                sudo cp -r "$repo_wip_path/$branch/"* \
+                sudo cp -r "$repo_wip_path/"* \
                     "$packages_path/$channel"
                 sudo chown -R """ + shlex.quote(uid) + """ "$packages_path"
             fi
@@ -147,6 +152,13 @@ class LocalJobServiceThread(threading.Thread):
         host = ("http://" + bpo.config.args.host + ":" +
                 str(bpo.config.args.port))
         wip_repo_path = bpo.config.args.repo_wip_path
+
+        if "_staging_" in branch:
+            branch_orig, name = bpo.repo.staging.branch_split(branch)
+            wip_repo_path += f"/staging/{name}/{branch_orig}"
+        else:
+            wip_repo_path += f"/{branch}"
+
         task_header = f"""
             export BPO_TOKEN_FILE="./token"
             export BPO_API_HOST={shlex.quote(host)}

bpo/jobs/sign_index.py
@@ -9,7 +9,7 @@ import bpo.helpers.job
 
 
 def run(arch, branch):
-    unsigned = "{}/{}/APKINDEX-symlink-repo.tar.gz".format(branch, arch)
+    unsigned = f"{arch}/APKINDEX-symlink-repo.tar.gz"
     uid = bpo.config.const.pmbootstrap_chroot_uid_user
     rsa = bpo.config.args.final_repo_key_name
     note = "Sign index: `{}/{}`".format(branch, arch)

bpo/repo/staging.py
@@ -184,10 +184,14 @@ def remove(branch):
             shutil.rmtree(path_name)
 
     # Remove wip repo dir
-    path = f"{bpo.config.args.repo_wip_path}/{branch}"
-    if os.path.exists(path):
-        logging.info(f"{branch}: remove {path}")
-        shutil.rmtree(path)
+    path_name = f"{bpo.config.args.repo_wip_path}/staging/{name}"
+    path_branch = f"{path_name}/{branch_orig}"
+    if os.path.exists(path_branch):
+        logging.info(f"{branch}: remove {path_branch}")
+        shutil.rmtree(path_branch)
+        if not any(os.scandir(path_name)):
+            logging.info(f"{branch}: remove {path_name}")
+            shutil.rmtree(path_name)
 
     # Remove from db
     session = bpo.db.session()

bpo/repo/wip.py
@@ -8,10 +8,24 @@ import subprocess
 import bpo.config.const
 import bpo.repo
 import bpo.repo.final
+import bpo.repo.staging
 
 
 def get_path(arch, branch):
-    return "{}/{}/{}".format(bpo.config.args.repo_wip_path, branch, arch)
+    repo_wip_path = bpo.config.args.repo_wip_path
+
+    if "_staging_" in branch:
+        branch_orig, name = bpo.repo.staging.branch_split(branch)
+        # Have the branch_orig + arch at the end of the path, so it is in the
+        # same format as for the original repositories and pmbootstrap is able
+        # to use it with "pmbootstrap --mirror-pmOS=..." with a staging URL
+        # like http://build.postmarketos.org/wip. If we
+        # used the branch name (master_staging_test) instead of the branch_orig
+        # (master), we would need to add additional complexity to pmbootstrap
+        # to figure out the correct full URLs.
+        return f"{repo_wip_path}/staging/{name}/{branch_orig}/{arch}"
+
+    return f"{repo_wip_path}/{branch}/{arch}"
 
 
 def do_keygen():

test/test_repo_staging.py
@@ -75,11 +75,11 @@ def test_remove(monkeypatch):
 
     branch_1 = "master_staging_test_branch"
     path_final_1 = f"{repo_final_path}/staging/test_branch/master"
-    path_wip_1 = f"{repo_wip_path}/{branch_1}"
+    path_wip_1 = f"{repo_wip_path}/staging/test_branch/master"
 
     branch_2 = "v23.06_staging_test_branch"
     path_final_2 = f"{repo_final_path}/staging/test_branch/v23.06"
-    path_wip_2 = f"{repo_wip_path}/{branch_2}"
+    path_wip_2 = f"{repo_wip_path}/staging/test_branch/v23.06"
 
     with bpo_test.BPOServer():
         try:

test/test_repo_wip.py
@@ -7,8 +7,26 @@ import bpo.db
 import bpo.repo
 import bpo.repo.wip
 
-import shutil
 import os
+import shutil
+import sys
+
+
+def test_get_path(monkeypatch):
+    monkeypatch.setattr(sys, "argv", ["bpo.py", "--repo-wip-path", "/repo-wip", "local"])
+    bpo.config.args.init()
+
+    func = bpo.repo.wip.get_path
+    arch = "x86_64"
+    branch = "master"
+    assert func(arch, branch) == "/repo-wip/master/x86_64"
+
+    branch = "master_staging_test"
+    assert func(arch, branch) == "/repo-wip/staging/test/master/x86_64"
+
+    # Reset
+    monkeypatch.setattr(sys, "argv", ["bpo.py", "local"])
+    bpo.config.args.init()
 
 
 def test_repo_wip_clean(monkeypatch):

test/test_zz_60_repo_staging.py
@@ -130,7 +130,7 @@ def test_build_publish_remove_staging_repo(monkeypatch, tmpdir):
     def bpo_ui_log(action, *args, **kwargs):
         assert action == "delete_staging_repo"
         assert not os.path.exists(f"{repo_final_path}/staging/test_1234")
-        assert not os.path.exists(f"{repo_wip_path}/{branch_staging}")
+        assert not os.path.exists(f"{repo_wip_path}/staging/test_1234/master")
         logging.info(" ### [part 8] done, stopping bpo server")
         bpo_test.stop_server()
 
@@ -152,7 +152,7 @@ def test_build_publish_remove_staging_repo(monkeypatch, tmpdir):
         logging.info(" ### [part 7] staging: remove")
         monkeypatch.setattr(bpo.ui, "log", bpo_ui_log)
         assert os.path.exists(f"{repo_final_path}/staging/test_1234")
-        assert os.path.exists(f"{repo_wip_path}/{branch_staging}")
+        assert os.path.exists(f"{repo_wip_path}/staging/test_1234/master")
         bpo_test.trigger.push_hook_gitlab(branch=branch_staging, background=True,
                                           after="0000000000000000000000000000000000000000")
 

commit ecba1bb3989053826de9cf098db866647d7c813f

job_services/sourcehut: fix overwriting job name
Don't overwrite the job name with the name of the staging branch (e.g.
sp1). This caused "get_secrets_by_job_name" to not add the proper
secrets for the final sign job, and so signing didn't work.

Fixes: d88ecb5e ("Fix WIP repo path for staging")

bpo/job_services/sourcehut.py
@@ -55,9 +55,9 @@ def get_manifest(name, tasks, branch):
     url_repo_wip_https = bpo.config.args.url_repo_wip_https + "/"
 
     if "_staging_" in branch:
-        branch_orig, name = bpo.repo.staging.branch_split(branch)
-        url_repo_wip_http += f"staging/{name}/"
-        url_repo_wip_https += f"staging/{name}/{branch_orig}/"
+        branch_orig, staging_name = bpo.repo.staging.branch_split(branch)
+        url_repo_wip_http += f"staging/{staging_name}/"
+        url_repo_wip_https += f"staging/{staging_name}/{branch_orig}/"
 
     branches = bpo.repo.staging.get_branches_with_staging()
     arches = " ".join(branches[branch]["arches"])