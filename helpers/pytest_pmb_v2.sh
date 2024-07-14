ea98debe97b577eb6b300b9429dea1e96da924fe
Oliver Smith <ollieparanoid@postmarketos.org>
2024-07-14 12:57:05 +0200

    helpers/pytest_pmb_v2: new script
    Add a helper script that can be used to run just one test with pytest,
    but using pmbootstrap v2.

---
+++ helpers/pytest_pmb_v2.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Wrapper for "pytest" that uses pmbootstrap v2, like in .ci/pytest-pmb-v2.sh
+PMBV2_DIR=../pmbootstrap_v2
+
+if ! [ -d "$PMBV2_DIR" ]; then
+	echo "ERROR: put pmbootstrap v2 here first: $PMBV2_DIR"
+	exit 1
+fi
+
+# Put pmbootstrap v2 into PATH
+TEMPBIN=$PWD/_temp_bpo_testsuite_bin
+rm -rf "$TEMPBIN"
+mkdir "$TEMPBIN"
+ln -s "$(realpath "$PMBV2_DIR")"/pmbootstrap.py "$TEMPBIN"/pmbootstrap
+export PATH="$TEMPBIN:$PATH"
+
+if ! pmbootstrap --version | grep -q '^2\.'; then
+	echo "ERROR: failed to put pmbv2 into PATH"
+	exit 1
+fi
+
+if ! [ -e "$PMBV2_DIR"/aports ]; then
+	ln -sf "$(pmbootstrap -q config aports)" "$PMBV2_DIR"/aports
+fi
+
+export BPO_PMA_MASTER_PMB_BRANCH="2.3.x"
+export BPO_PMA_STAGING_PMB_BRANCH="2.3.x"
+export BPO_PMB_PATH="$(realpath "$PWD/../pmbootstrap_v2")"
+
+pytest "$@"
